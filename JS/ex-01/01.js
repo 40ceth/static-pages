445a243c210c0947752bbc168d7abca47c6e9b48
40ceth <zxasqwedc402@gmail.com>
2021-03-30 18:25:30 +0800

    0.08

---
+++ JS/ex-01/01.js
@@ -0,0 +1,13 @@
+var person = new Object();
+person["firstname"]='zack';
+person['lastname']="alice";
+var firstnameproperty="firstname";
+
+console.log(person);
+console.log(person[firstnameproperty]);
+// console.log(beet.firstname);
+// console.log(beet.lastname);
+// console.log(beet["firstname"]);
+// console.log(beet["lastname"]);
+// console.log(beet["who"]);
+
